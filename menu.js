9c58e7821e2bb36542b99eb055a3325a4ab65535
oskar <oskarornvs@gmail.com>
2018-11-07 09:30:23 +0100

    Initial commit

---
+++ menu.js
@@ -0,0 +1,62 @@
+/*
+var greeting = document.getElementById('greeting');
+
+greeting.addEventListener('click', changeGreeting);
+
+
+function changeGreeting() {
+    greeting.innerHTML = 'Good bye';
+}
+*/
+//meny elements
+
+/*
+var menyOne = document.getElementById('one');
+var menyTwo = document.getElementById('two');
+var menyThree = document.getElementById('three');
+var menyFour = document.getElementById('four');
+
+
+//content elements
+var contentOne = document.getElementById('one_content');
+var contentTwo = document.getElementById('two_content');
+var contentThree = document.getElementById('three_content');
+var contentFour = document.getElementById('four_content');
+
+var currentContent = contentOne;
+
+
+menyOne.addEventListener('click', clickedOne);
+menyTwo.addEventListener('click', clickedTwo);
+menyThree.addEventListener('click', clickedThree);
+menyFour.addEventListener('click', clickedFour);
+
+function clickedOne() {
+    currentContent.style.display = 'none';
+    contentOne.style.display = 'inline';
+    currentContent = contentOne;
+
+}
+
+function clickedTwo() {
+    currentContent.style.display = 'none';
+    contentTwo.style.display = 'inline';
+    currentContent = contentTwo;
+
+}
+
+function clickedThree() {
+    currentContent.style.display = 'none';
+    contentThree.style.display = 'inline';
+    currentContent = contentThree;
+
+}
+
+function clickedFour() {
+    currentContent.style.display = 'none';
+    contentFour.style.display = 'inline';
+    currentContent = contentFour;
+
+}
+*/
+
---
+++ menu2.js
@@ -0,0 +1,34 @@
+/*
+var el = document.getElementById('clickMe');
+
+el.addEventListener('click', clicked);
+
+function clicked() {
+    el.classList.toggle('highlight');
+}
+*/
+
+/*
+var menu = document.getElementsByClassName('menu');
+var currentMenu = document.getElementById('one');
+var currentContent = document.getElementById('one_content');
+
+for(let i = 0; i < menu.length; i++){
+
+    menu[i].addEventListener('click', clickedMenu);
+}
+
+function clickedMenu() {
+
+    currentMenu.classList.remove('highlight');
+    this.classList.add('highlight');
+    currentMenu = this;
+
+
+    var content = document.getElementById(this.id + '_content');
+    currentContent.style.display = 'none';
+    currentContent = content;
+    content.style.display = 'inline';
+}
+*/
+
